8ef3686e243d6eb15050067a0bab3c2daa94d351
GuillaumeGenier <91501194+GuillaumeGenier@users.noreply.github.com>
2022-04-28 15:34:35 -0500

    redefining to use a docker volume for static config and an overlay netowrk to maintain the grid

--- create-storage-nogc.sh
+++ create-storage-nogc.sh
@@ -2,19 +2,23 @@
 #This bash script start a docker swarm stack of tahoe-LAFS storage node.
 #In order to be a valid target for the deployement, the label tahoe.storage = true must be set on docker nodes
 
-SERVICE_URL=test_url
-TAHOE_CFG_DIR=/opt/tahoe/storage/config
+TAHOE_LOG_DIR=/opt/tahoe/storage/log
 TAHOE_STORAGE_DIR=/opt/tahoe/storage/data
 TAHOE_TUB_PORT=20000
 
-export SERVICE_URL
-export TAHOE_CFG_DIR
+TAHOE_INTRODUCER_FURL=pb://3b36flsair4yqc2xi63jvnzzwfiy23cl@tcp:test:38771/jpwpdlzkpqqpyziqanjkhtoewp7bd6ni
+
+export TAHOE_LOG_DIR
 export TAHOE_STORAGE_DIR
 export TAHOE_TUB_PORT
+export TAHOE_INTRODUCER_FURL
 
-mkdir -p ${TAHOE_CFG_DIR}
+
+mkdir -p ${TAHOE_LOG_DIR}
 mkdir -p ${TAHOE_STORAGE_DIR}
 
-docker stack deploy tahoe_ -c $PWD/compose_docker/compose_init_storage.yml
+docker stack deploy tahoe -c $PWD/compose_docker/compose_init_storage.yml
 
-docker stack deploy tahoe_ -c $PWD/compose_docker/compose_run_storage.yml
+sleep 3
+
+docker stack deploy tahoe -c $PWD/compose_docker/compose_run_storage.yml
