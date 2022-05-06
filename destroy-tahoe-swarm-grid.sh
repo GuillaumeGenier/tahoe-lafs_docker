429922131423d4961891685d4d027fb1866a92e6
Guillaume Génier <91501194+GuillaumeGenier@users.noreply.github.com>
2022-05-06 15:45:10 -0400

    Corrected client balancing in compose and default encoding parameters

--- destroy-tahoe-swarm-grid.sh
+++ destroy-tahoe-swarm-grid.sh
@@ -2,5 +2,4 @@
 #This bash script destroy a docker swarm stack of tahoe-LAFS storage nodes.
 #The logs volume need to be cleaned by hand oin the nodes in the swarm for now.
 
-docker stack rm tahoe-grid 
-docker network rm tahoe-grid-network
+docker stack rm tahoe-grid
